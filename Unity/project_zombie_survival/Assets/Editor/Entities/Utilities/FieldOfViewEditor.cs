68af9bd8b28347c9cdf0113cefc8c553736bdaf8
Brendan Chapman <55817314+ChappyGames@users.noreply.github.com>
2020-05-23 03:40:10 -0500

    Organized Entity Script Files and Added Zombies
    - Added basic zombie pathfinding
    - Added basic zombie field of view

---
+++ Unity/project_zombie_survival/Assets/Editor/Entities/Utilities/FieldOfViewEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor {
+
+    private void OnSceneGUI() {
+        FieldOfView lFov = (FieldOfView)target;
+        Handles.color = Color.white;
+        
+        Vector3 lViewAngleA = lFov.GetVectorFromAngle(-lFov.viewAngle / 2, false);
+        Vector3 lViewAngleB = lFov.GetVectorFromAngle(lFov.viewAngle / 2, false);
+
+        Handles.DrawWireArc(lFov.transform.position, Vector3.up, lViewAngleA, lFov.viewAngle, lFov.viewRadius);
+
+        Handles.DrawLine(lFov.transform.position, lFov.transform.position + lViewAngleA * lFov.viewRadius);
+        Handles.DrawLine(lFov.transform.position, lFov.transform.position + lViewAngleB * lFov.viewRadius);
+
+        Handles.color = Color.red;
+        foreach(Transform aVisibleTarget in lFov.visibleTargets) {
+            Handles.DrawLine(lFov.transform.position, aVisibleTarget.position);
+        }
+    }
+
+}
---
+++ Unity/project_zombie_survival/Assets/Scripts/Entities/Zombie/ZombieController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(FieldOfView))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class ZombieController : MonoBehaviour {
+
+    private FieldOfView fov;
+    private NavMeshAgent nav;
+
+    private PlayerController targetPlayer;
+
+    private void Awake() {
+        fov = GetComponent<FieldOfView>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+
+        if (targetPlayer == null) {
+            
+            // Fetch a visible player if any.
+            for (int i = 0; i < fov.visibleTargets.Count; i++) {
+                PlayerController lPlayer = fov.visibleTargets[i].GetComponent<PlayerController>();
+                if (lPlayer != null) {
+                    targetPlayer = lPlayer;
+                    break;
+                }
+            }
+        } else {
+
+            // Navigate to target player.
+            nav.destination = targetPlayer.transform.position;
+        }
+    }
+}
